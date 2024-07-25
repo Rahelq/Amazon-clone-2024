cf1284256daa67911a3857c6bdad29d052da5d66
Rahelq <belayrahel500@gmail.com>
2024-07-25 23:32:00 +0300

    baseURL changed

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-edd82/us-central1/api",
+  //local instance of firebase functions
+  // baseURL: "http://127.0.0.1:5001/clone-edd82/us-central1/api",
+
+  //deployed version of firebase function on render.com
+  baseURL: "https://amazon-api-deploy-7bz1.onrender.com/",
+
 });
